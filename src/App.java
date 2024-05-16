c5e125178cb8e0e73ccfdc82753bc637c77308da
Shymon2 <aduy2200@gmail.com>
2024-05-17 00:45:11 +0700

    first commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import GameMain.GameMainTest;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        GameMainTest gm = new GameMainTest();
+        gm.Run();
+    }
+}
